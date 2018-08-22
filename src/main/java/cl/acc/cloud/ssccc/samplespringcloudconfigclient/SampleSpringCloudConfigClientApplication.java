85d676617f1a19898fee9fd85dcdc1633b90827d
Alexander Castillo <acastillo@thoughworks.com>
2018-08-22 10:45:56 -0300

    Add config spring config client

---
+++ src/main/java/cl/acc/cloud/ssccc/samplespringcloudconfigclient/SampleSpringCloudConfigClientApplication.java
@@ -0,0 +1,12 @@
+package cl.acc.cloud.ssccc.samplespringcloudconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringCloudConfigClientApplication.class, args);
+	}
+}
